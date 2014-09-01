98e746f2aa48abeb9432566be72cc8aa2c321ff1
John Hiott <jhiott@oolcay.com>
2014-09-01 09:20:19 -0400

    place model

---
+++ app/src/main/java/com/johnhiott/sundayfunday/models/Place.java
@@ -0,0 +1,31 @@
+package com.johnhiott.sundayfunday.models;
+
+public class Place {
+
+  private String name;
+  private String address;
+  private double lat;
+  private double lon;
+  private String license;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public double getLat() {
+    return lat;
+  }
+
+  public double getLon() {
+    return lon;
+  }
+
+  public String getLicense() {
+    return license;
+  }
+
+}
