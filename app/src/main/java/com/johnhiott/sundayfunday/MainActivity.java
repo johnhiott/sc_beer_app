b002fb27c0f7b072c319556ce04ce8c45997273b
John Hiott <jhiott@oolcay.com>
2014-09-01 21:50:39 -0400

    add nav drawer

--- app/src/main/java/com/johnhiott/sundayfunday/MainActivity.java
+++ app/src/main/java/com/johnhiott/sundayfunday/MainActivity.java
@@ -1,6 +1,8 @@
 package com.johnhiott.sundayfunday;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import com.google.android.gms.maps.GoogleMap;
@@ -8,8 +10,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.location.Location;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
@@ -36,18 +42,96 @@ public class MainActivity extends FragmentActivity implements
   private String mResponse;
   private Place[] places;
   private Gson gson;
+  private String[] navTitles;
+  private ListView mDrawerList;
+  private ActionBarDrawerToggle mDrawerToggle;
+  private CharSequence mDrawerTitle;
+  private CharSequence mTitle;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    //TODO: lot's of error handling for locations and play services missing
+
+    mTitle = getTitle();
+    mDrawerTitle = "Select Type of Location";
+
     mContext = getApplicationContext();
+
+    navTitles = getResources().getStringArray(R.array.nav_options);
+
+    mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+    // Set the adapter for the list view
+    mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+          android.R.layout.simple_list_item_1, navTitles));
+
+    DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    mDrawerToggle = new ActionBarDrawerToggle(
+          this,                  /* host Activity */
+          mDrawerLayout,         /* DrawerLayout object */
+          R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+          R.string.drawer_open,  /* "open drawer" description */
+          R.string.drawer_close  /* "close drawer" description */
+    ) {
+
+      /** Called when a drawer has settled in a completely closed state. */
+      public void onDrawerClosed(View view) {
+        super.onDrawerClosed(view);
+        getActionBar().setTitle(mTitle);
+      }
+
+      /** Called when a drawer has settled in a completely open state. */
+      public void onDrawerOpened(View drawerView) {
+        super.onDrawerOpened(drawerView);
+        getActionBar().setTitle(mDrawerTitle);
+      }
+    };
+
+    // Set the drawer toggle as the DrawerListener
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+    getActionBar().setHomeButtonEnabled(true);
+
+
+    // Set the drawer toggle as the DrawerListener
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
 
     mLocationClient = new LocationClient(this,this,this);
     mLocationClient.connect();
 
   }
+
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    // Sync the toggle state after onRestoreInstanceState has occurred.
+    mDrawerToggle.syncState();
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    mDrawerToggle.onConfigurationChanged(newConfig);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Pass the event to ActionBarDrawerToggle, if it returns
+    // true, then it has handled the app icon touch event
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    // Handle your other action bar items...
+
+    return super.onOptionsItemSelected(item);
+  }
+
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,17 +140,6 @@ public class MainActivity extends FragmentActivity implements
     return true;
   }
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    if (id == R.id.action_settings) {
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
 
   @Override
   public void onConnected(Bundle bundle) {
